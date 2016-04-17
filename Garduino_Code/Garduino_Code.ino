6a35a94b482d8db995895504272d6d305082ffc6
Arne Malcharowitz <arne.malcharowitz@campus.tu-berlin.de>
2016-04-17 18:00:44 +0200

    humidity & DHT11 code added

--- Garduino_Code/Garduino_Code.ino
+++ Garduino_Code/Garduino_Code.ino
@@ -4,17 +4,59 @@
   Ardunio IDE 1.6.6
 */
 
+#include "DHT.h"
+#include <Servo.h>
+
+//Festlegen der Pinbelegung
+#define DHTPIN 2 //Pinbelegung für DHT11 Sensor
+#define DHTTYPE DHT11
+const int pin_humidity_sensor = A0;
+
+//Initialize DHT sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+//Variablen
+int humidity_soil = 0;
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  Serial.println("Serial Monitor online");
+  dht.begin();
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(10);        // delay in between reads for stability
+  // HUMIDITY SOIL: read the input on analog pin 0:
+  humidity_soil = analogRead(pin_humidity_sensor);
+  humidity_soil = map(humidity_soil, 0, 600, 0, 100);
+  humidity_soil = constrain(humidity_soil, 0, 100);
+  int sensorValue = map(humidity_soil, 0, 100, 0, 255);
+
+  // TEMP+HUMIDITY AIR: read the input on digital pin 2:
+  float humidity_air = dht.readHumidity();
+  float temperature_air = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity_air) || isnan(temperature_air)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  //Fade the LED using the mapped value
+  analogWrite(9, sensorValue);
+
+  // print out the values you read:
+  Serial.print("Bodenfeuchtigkeit: ");
+  Serial.print(humidity_soil);
+  Serial.print(" %;");
+
+  Serial.print("Luftfeuchtigkeit: ");
+  Serial.print(humidity_air);
+  Serial.print(" %;");
+
+  Serial.print("Lufttemperatur: ");
+  Serial.print(temperature_air);
+  Serial.println(" °C");
+  delay(2000);        // delay in between reads for stability
 }
